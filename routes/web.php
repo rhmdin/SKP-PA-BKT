0490851c3df1ff0308b8881c6459b71ff3153040
dina <89837465+rhmdin@users.noreply.github.com>
2023-01-19 12:05:16 +0700

    nambah hal login

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts/sidebar');
+    return view('login');
 });
 
 Route::get('/indikator-kinerja',  function () {
